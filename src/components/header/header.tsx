8136fd6f9cdf325a25f20da0bae6abb5306c7dad
bryceberczik <berczikbryce@gmail.com>
2024-10-19 20:32:06 -0500

    major changes added to home page and need to do more styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import './header.css';
+import BrandLogo from '../../images/converted_image.png';
 
 const Header = () => {
 
@@ -6,16 +7,16 @@ const Header = () => {
         <div className='header-container'>
             <header>
                 <div className="header-left">
-                    <h1>LOGO</h1>
+                    <img src={BrandLogo} alt="brand name"  width='75'/>
                 </div>
                 <div className="header-center">
-                <h1>HOME</h1>
-                <h1>LOCATION & HOURS</h1>
-                <h1>MENU</h1>
-                <h1>ORDER ONLINE</h1>
+                <h1 className='header-text'>HOME</h1>
+                <h1 className='header-text'>LOCATION & HOURS</h1>
+                <h1 className='header-text'>MENU</h1>
+                <h1 className='header-text'>ORDER ONLINE</h1>
                 </div>
                 <div className="header-right">
-                    <h1>CONTACT US</h1>
+                    <h1 className='header-text'>CONTACT US</h1>
                 </div>
             </header>
         </div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,74 @@
 import "../styles/home.css";
-import BrandLogo from "../images/converted_image.png";
+import firstPhoto from "../images/foodtruck_1.jpeg";
+import taco from '../images/tacos.jpg';
 
 const Home = () => {
   return (
-    <div>
-
+    <div className="home-container">
+      <div className="first-section">
+        <div className="first-section-intro">
+          <h1 className="home-intro">Welcome to Mama Irma's,</h1>
+          <h1 className="home-intro">Enjoy your stay.</h1>
+        </div>
+        <div className="about-us">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
+            quas magnam animi reiciendis maiores assumenda error non numquam
+            aperiam atque nihil, perspiciatis explicabo mollitia iusto debitis
+            delectus? Natus, sequi doloremque?
+          </p>
+        </div>
+      </div>
+      <div className="serving-austin">
+        <h2>
+          Serving authentic Mexican cuisine to the heart of Austin, Texas.
+        </h2>
+      </div>
+      <div className="food-examps">
+        <div className="line-1">
+          <img src={taco} alt="taco" />
+          <div className="line-1-text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae vitae error quasi? Deserunt blanditiis quis fugit, quidem illum debitis. Ea, delectus quibusdam. Commodi fugiat possimus facilis quas libero, impedit atque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda numquam perspiciatis nulla ex nisi corrupti consequatur sed iste omnis beatae. Nostrum, tempora nesciunt consequuntur eveniet id animi quas quos dolorem.</p>
+          </div>
+        </div>
+        <div className="line-1">
+        <div className="line-1-text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae vitae error quasi? Deserunt blanditiis quis fugit, quidem illum debitis. Ea, delectus quibusdam. Commodi fugiat possimus facilis quas libero, impedit atque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda numquam perspiciatis nulla ex nisi corrupti consequatur sed iste omnis beatae. Nostrum, tempora nesciunt consequuntur eveniet id animi quas quos dolorem.</p>
+          </div>
+          <img src={taco} alt="taco" />
+        </div>
+        <div className="line-1">
+          <img src={taco} alt="taco" />
+          <div className="line-1-text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae vitae error quasi? Deserunt blanditiis quis fugit, quidem illum debitis. Ea, delectus quibusdam. Commodi fugiat possimus facilis quas libero, impedit atque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda numquam perspiciatis nulla ex nisi corrupti consequatur sed iste omnis beatae. Nostrum, tempora nesciunt consequuntur eveniet id animi quas quos dolorem.</p>
+          </div>
+        </div>
+      </div>
+      <div className="extra-home-btns">
+        <div className="find-us">
+          <h1>Find us here</h1>
+          <div className="click-location">
+            <h1>LOCATION</h1>
+          </div>
+        </div>
+        <div className="menu-extra">
+          <h1>See our menu</h1>
+          <div className="click-menu">
+            <h1>MENU</h1>
+          </div>
+        </div>
+      </div>
+      <div className="friendly-staff">
+        <h1>Our friendly staff</h1>
+      </div>
+      <div className="first-home-photo">
+        <img src={firstPhoto} alt="foodtruck" />
+        <img src={firstPhoto} alt="foodtruck" />
+        <img src={firstPhoto} alt="foodtruck" />
+      </div>
+      <div className="reviews">
+        <h1>Reviews</h1>
+      </div>
     </div>
   );
 };
